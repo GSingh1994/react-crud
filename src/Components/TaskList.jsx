0c5e1f51c9637fa618887ed6b00a2d3b488ed258
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-10-29 15:02:46 -0500

    add react-controlled form to save a task

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,9 +1,22 @@
 import Task from "./Task";
 import "./TaskList.scss";
+
+import { useState } from "react";
+
 const TaskList = () => {
+  const [task, setTask] = useState("");
+
+  const addNewTask = (e) => {
+    e.preventDefault();
+    console.log(task);
+  };
   return (
     <div className="TaskList">
       <h1>Tasklist</h1>
+      <form>
+        <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
+        <button onClick={addNewTask}>Add</button>
+      </form>
       <Task />
     </div>
   );
